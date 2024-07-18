1bede88631e338d29bbadd6ed4889a35f481baae
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-18 15:15:47 +0700

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+module.exports = pool;
