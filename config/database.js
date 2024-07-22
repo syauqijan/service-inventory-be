e817d4f4618fd7fb4759e04a655dc5a2f0e853ea
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-22 14:51:56 +0700

    feat: add env

--- config/database.js
+++ config/database.js
@@ -1,12 +1,13 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-
-// const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql'
-// });
-const db = new Sequelize('tes_database','root','123456',{
-  host: 'localhost',
-  dialect: 'mysql'
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+        max: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 5
+    }
 });
+
 export default db;
