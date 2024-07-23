96188535a049699be25524a1beffc05707366239
syauqijan <13521014@std.stei.itb.ac.id>
2024-07-23 09:12:07 +0700

    feat: update config db

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: 3307,
     dialect: 'mysql',
     pool: {
         max: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 5
